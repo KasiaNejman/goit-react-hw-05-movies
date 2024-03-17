e076b96038967ab5aabdab18b860c4abd5de3f51
KasiaNejman <139686065+KasiaNejman@users.noreply.github.com>
2024-03-17 14:29:56 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+import { HashRouter as Router } from 'react-router-dom'; // Zmieniono na HashRouter
+import App from './components/App';
+import "./main.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Router> {/* Opakowanie komponentu App w Router */}
+      <App />
+    </Router>
   </React.StrictMode>
 );
