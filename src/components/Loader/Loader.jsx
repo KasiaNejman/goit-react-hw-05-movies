5bde34d47d9b520e2e9910da586c952cdd83b6cd
KasiaNejman <139686065+KasiaNejman@users.noreply.github.com>
2024-06-07 16:49:40 +0200

    Update Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { MagnifyingGlass } from "react-loader-spinner";
+import React from 'react';
+import { MagnifyingGlass } from 'react-loader-spinner';
 
 const Loader = () => {
   <MagnifyingGlass
@@ -8,21 +8,21 @@ const Loader = () => {
     width="80"
     ariaLabel="MagnifyingGlass-loading"
     wrapperStyle={{
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      height: "100vh",
-      margin: "0 auto",
-      position: "fixed",
-      top: "0",
-      left: "0",
-      right: "0",
-      bottom: "0",
-      zIndex: "9999",
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      margin: '0 auto',
+      position: 'fixed',
+      top: '0',
+      left: '0',
+      right: '0',
+      bottom: '0',
+      zIndex: '9999',
     }}
     wrapperClass="MagnifyingGlass-wrapper"
-    glassColor="#c0efff"
-    color="#949293"
+    glassColor="#F0F8FF"
+    color="#2F4F4F"
   />;
 };
 
